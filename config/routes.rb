9c102354cf23861afe44a6e619aaa718ab715e45
Jonathan Tsang <jw.tsang@gmail.com>
2022-08-08 03:14:08 -0300

    refactor(routes): add new root, trim layout template

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'welcome#index'
+  root 'reqres#index'
+
+  get 'welcome', to: 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
